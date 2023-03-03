c00084a6c499ace2a2a365ae69ef2e39e7b8dc75
Choi-Tae-min <123718568+Choi-Tae-min@users.noreply.github.com>
2023-03-03 16:55:26 +0900

    Update exist.py

--- exist.py
+++ exist.py
@@ -1,6 +1,6 @@
 import os
-
-path = 'C:\\Users\\user\\monodepth2_remake\\data\\ep4\\color\\image_-1.png'
+#if you want file exist, use this
+path = 'data_path'
 
 if os.path.exists(path):
     print("File exists!")
